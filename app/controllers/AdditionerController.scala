4e2fb523228da57220275d9d976645e581247c75
Simonchelbc <simon.lebail-collet@epfl.ch>
2017-05-25 21:45:59 +0200

    make the webserver simulate a long computation time for the addition

--- app/controllers/AdditionerController.scala
+++ app/controllers/AdditionerController.scala
@@ -1,18 +1,19 @@
 package controllers
 
 
-import util.parsing.combinator.RegexParsers
-import scala.collection.mutable.Buffer
+import play.api.libs.concurrent.Execution.Implicits._ //for the implicit execution context necessary for simulating a long computation time of an addition
 import javax.inject._
+import scala.concurrent.Future
 import play.api._
 import play.api.mvc._
-import play.twirl.api._
 import play.api.libs.json._
 
 
 
 
-case class TwoDVector(x: Double, y: Double)
+case class TwoDVector(x: Double, y: Double) {
+	def add: Double = x + y
+}
 /**
  * This controller creates `Action`s to handle HTTP requests to the
  * application's home page.
@@ -27,17 +28,23 @@ class AdditionerController extends Controller {
 	 * that need the addition of two numbers x and y contained in a JSON 
 	 * object.
  	 */
-	def add() = Action(BodyParsers.parse.json) { request => 
+	def add() = Action.async(BodyParsers.parse.json) { request => 
 		val textResult: JsResult[TwoDVector] = 
 			request.body.validate[TwoDVector](vectorReads)
 
-		textResult.map { v => 
-			Ok((v.x + v.y).toString)
+		val addRes: Result = textResult.map { v => 
+			Ok(v.add.toString) //the result as a string
 		}.getOrElse {
 			BadRequest(
 				"Please provide a json containing x and y\n"+
-				"attributes that must be numbers. You provided:\n"+request.body
+				"attributes that must be numbers. You provided:\n"+
+				request.body
 			)
+		}
+
+		Future {
+			Thread.sleep(2000) //simulating a long computation time
+			addRes
 		}
 	}
 
